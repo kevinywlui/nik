2637eeafafaf63b9a7d66e2bb57d7d9b42102cc5
kevin lui <kevinywlui@gmail.com>
2020-02-22 17:39:42 -0800

    Change behavior when no arguments passed to zsh alias

--- nik.zsh
+++ nik.zsh
@@ -1,11 +1,17 @@
 # zsh plugin for nik
 
+# hook to update score after each cd
 __nik_update() {
         nik update `pwd`
 }
+add-zsh-hook chpwd __nik_update.
 
+# single-letter alias to access nik
 j() {
-        cd `nik get $@`
+        if [[ $# == 0 ]] then
+                `nik list -s`
+        else
+                cd `nik get $@`
+        fi
 }
 
-add-zsh-hook chpwd __nik_update
